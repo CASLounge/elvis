f00d27da80ddebdcea00cee9965e16b13f25a3ea
devzana <lozanascbusiness@gmail.com>
2022-07-29 22:34:08 +0800

    Changed endpoint's http methods according to their operations

--- src/api/v1/controllers/Post/postController.ts
+++ src/api/v1/controllers/Post/postController.ts
@@ -2,43 +2,48 @@ import { NextFunction, Request, Response, Router } from 'express'
 import { useJWT } from '../../utilities/useJWT'
 export const postController: Router = Router()
 
+// Middleware for protecting APIs with Bearer Token pattern
 const { verify } = useJWT()
 
 // TODO:
 // Like Post
-postController.get('/like', verify, async (req: Request, res: Response, next: NextFunction) => {
+postController.put('/like', verify, async (req: Request, res: Response, next: NextFunction) => {
   try {
     res.status(200).send({ data: 'posts are working 🥳' })
   } catch (error) {
     next(error)
   }
 })
+
 // New Post
-postController.get('/new', verify, async (req: Request, res: Response, next: NextFunction) => {
+postController.post('/new', verify, async (req: Request, res: Response, next: NextFunction) => {
   try {
     res.status(200).send({ data: 'posts are working 🥳' })
   } catch (error) {
     next(error)
   }
 })
+
 // Delete Post
-postController.get('/delete', verify, async (req: Request, res: Response, next: NextFunction) => {
+postController.delete('/delete', verify, async (req: Request, res: Response, next: NextFunction) => {
   try {
     res.status(200).send({ data: 'posts are working 🥳' })
   } catch (error) {
     next(error)
   }
 })
+
 // Edit Post
-postController.get('/edit', verify, async (req: Request, res: Response, next: NextFunction) => {
+postController.patch('/edit', verify, async (req: Request, res: Response, next: NextFunction) => {
   try {
     res.status(200).send({ data: 'posts are working 🥳' })
   } catch (error) {
     next(error)
   }
 })
-// unpublish Post
-postController.get('/unpublish', verify, async (req: Request, res: Response, next: NextFunction) => {
+
+// Unpublish Post
+postController.put('/unpublish', verify, async (req: Request, res: Response, next: NextFunction) => {
   try {
     res.status(200).send({ data: 'posts are working 🥳' })
   } catch (error) {
