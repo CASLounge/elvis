3d7488ad881283477bb17480a96e9e96419ab10b
devzana <lozanascbusiness@gmail.com>
2022-08-14 14:24:37 +0800

    Fixed @ts-ignore by typecasting the env variables for port numbers

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,12 @@ import { app } from './api/v1/app'
 
 dotenv.config()
 
-const port = process.env.PORT || 3001
+// The difference between PORT and SERVER_PORT is
+// SERVER_PORT = Development and Staging development environment
+// PORT = is the default heroku env var which is good for preview or production
+// Bruh...
+const port: number = Number(process.env.PORT!) || Number(process.env.SERVER_PORT!)
 
-// @ts-ignore
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server is running @ port ${port}`)
 })
