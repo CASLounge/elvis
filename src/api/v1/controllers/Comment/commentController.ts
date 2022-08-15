52ce8691fd7a871558ab03ea42b0417c8700a1b9
devzana <lozanascbusiness@gmail.com>
2022-08-15 23:27:10 +0800

    Tested role verifier middlware to commentsController

--- src/api/v1/controllers/Comment/commentController.ts
+++ src/api/v1/controllers/Comment/commentController.ts
@@ -1,13 +1,24 @@
 import { NextFunction, Request, Response, Router } from 'express'
 import { useJWT } from '../../utilities/useJWT'
+import { useRoles } from '../../utilities/useRoles'
 export const commentController: Router = Router()
 
 // Middleware for protecting APIs with Bearer Token pattern
 const { verify } = useJWT()
+const { roles } = useRoles()
 
 // TODO:
+commentController.get('/view', [verify, roles('ADMIN', 'STUDENT', 'MOD')], async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    // const { id } = req.query
+    res.status(200).send({ data: 'An admin, student and, mods should be able to see this! 🥳' })
+  } catch (error) {
+    next(error)
+  }
+})
+
 // Like comment
-commentController.get('/reply', verify, async (req: Request, res: Response, next: NextFunction) => {
+commentController.get('/reply', [verify, roles('ADMIN', 'STUDENT', 'MOD')], async (req: Request, res: Response, next: NextFunction) => {
   try {
     res.status(200).send({ data: 'comments working 🥳' })
   } catch (error) {
@@ -16,7 +27,7 @@ commentController.get('/reply', verify, async (req: Request, res: Response, next
 })
 
 // Like comment
-commentController.get('/like', verify, async (req: Request, res: Response, next: NextFunction) => {
+commentController.get('/like', [verify, roles('ADMIN', 'STUDENT', 'MOD')], async (req: Request, res: Response, next: NextFunction) => {
   try {
     res.status(200).send({ data: 'comments working 🥳' })
   } catch (error) {
@@ -25,7 +36,7 @@ commentController.get('/like', verify, async (req: Request, res: Response, next:
 })
 
 // New comment
-commentController.get('/new', verify, async (req: Request, res: Response, next: NextFunction) => {
+commentController.get('/new', [verify, roles('ADMIN', 'STUDENT', 'MOD')], async (req: Request, res: Response, next: NextFunction) => {
   try {
     res.status(200).send({ data: 'comments working 🥳' })
   } catch (error) {
@@ -34,16 +45,16 @@ commentController.get('/new', verify, async (req: Request, res: Response, next: 
 })
 
 // Delete comment
-commentController.get('/delete', verify, async (req: Request, res: Response, next: NextFunction) => {
+commentController.get('/delete', [verify, roles('ADMIN', 'MOD')], async (req: Request, res: Response, next: NextFunction) => {
   try {
-    res.status(200).send({ data: 'comments working 🥳' })
+    res.status(200).send({ data: 'Only the admin should be able to receive this! 🥳' })
   } catch (error) {
     next(error)
   }
 })
 
 // Edit comment
-commentController.get('/edit', verify, async (req: Request, res: Response, next: NextFunction) => {
+commentController.get('/edit', [verify, roles('ADMIN', 'STUDENT', 'MOD')], async (req: Request, res: Response, next: NextFunction) => {
   try {
     res.status(200).send({ data: 'comments working 🥳' })
   } catch (error) {
